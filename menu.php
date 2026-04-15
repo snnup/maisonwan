02638d14f76548735565c8890349e370ec0c7527
Felipe <felipewilkersousa@gmail.com>
2026-04-14 21:12:40 -0300

    tirando as setas do nav

--- menu.php
+++ menu.php
@@ -1,13 +1,13 @@
         <nav>
             <ul>
-        <li><a href="/haute-couture">HAUTE COUTURE<i class="ph ph-caret-right"></i></a></li>
-        <li><a href="/fashion">FASHION<i class="ph ph-caret-right"></i></a></li>
-        <li><a href="/high-jewelry">HIGH JEWELRY<i class="ph ph-caret-right"></i></a></li>
-        <li><a href="/fine-jewelry">FINE JEWELRY<i class="ph ph-caret-right"></i></a></li>
-        <li><a href="/watches">WATCHES<i class="ph ph-caret-right"></i></a></li>
-        <li><a href="/N5">N°5<i class="ph ph-caret-right"></i></a></li>
-        <li><a href="/eyewear">EYEWEAR<i class="ph ph-caret-right"></i></a></li>
-        <li><a href="/men">MEN <i class="ph ph-caret-right"></i></a></li>
-        <li><a href="/woman">WOMAN <i class="ph ph-caret-right"></i></a></li>
+        <li><a href="/haute-couture">HAUTE COUTURE</a></li>
+        <li><a href="/fashion">FASHION</a></li>
+        <li><a href="/high-jewelry">HIGH JEWELRY</a></li>
+        <li><a href="/fine-jewelry">FINE JEWELRY</a></li>
+        <li><a href="/watches">WATCHES</a></li>
+        <li><a href="/N5">N°5</a></li>
+        <li><a href="/eyewear">EYEWEAR</a></li>
+        <li><a href="/men">MEN</a></li>
+        <li><a href="/woman">WOMAN</a></li>
             </ul>
         </nav>
